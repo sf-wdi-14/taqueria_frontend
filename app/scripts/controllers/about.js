cbe50a21a95d554b058890cf2c62ce150970defe
Dennis Charles Hackethal <dennis@generalassemb.ly>
2015-02-04 09:56:09 -0800

    Create app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name taqueriaFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the taqueriaFrontendApp
+ */
+angular.module('taqueriaFrontendApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name taqueriaFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the taqueriaFrontendApp
+ */
+angular.module('taqueriaFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
